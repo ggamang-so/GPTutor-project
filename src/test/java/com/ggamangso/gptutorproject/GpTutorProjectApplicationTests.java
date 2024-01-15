f9bce79be374ff03212b2a88a8cbf798af36fcb9
ggamangso1 <ggamangso@gmail.com>
2024-01-15 20:01:45 +0900

    #2 - 스프링 부트로 프로젝트 시작
    * `.gitignore` 설정
    * 프로젝트에 필요한 의존성 주입
    * `README.md` 기본 골격 작성

---
+++ src/test/java/com/ggamangso/gptutorproject/GpTutorProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggamangso.gptutorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpTutorProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
