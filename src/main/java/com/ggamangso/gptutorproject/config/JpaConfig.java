a2318fba57ca1cdb879301ffcc60905d237ea0da
ggamangso1 <ggamangso@gmail.com>
2024-01-18 16:28:17 +0900

    #8 -  Spring Data JPA 속성과 설정
    * jpa auditing 기능을 쓰기 위해 JpaConfig 추가
    * db접근과 개발 편의를 위한 각종 부트 프로퍼티 적용
    * 민감정보는 환경변수에 담아서 주입

---
+++ src/main/java/com/ggamangso/gptutorproject/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package com.ggamangso.gptutorproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("gptutor"); //임시로 지정, 추후 수정
+    }
+
+}
