e497f2730d4bc9493b4c87eaddde415b3c98490c
ggamang-so <ggamangso@gmail.com>
2024-02-11 21:15:26 +0900

    MessageType of static method 추가
    외부 API에서 답변을 받을 때 role에 String 형태로 들어오는 데 그것을 enum 타입으로 전환하여
    저장하기 위한 of 메서드 추가

--- src/main/java/com/ggamangso/gptutorproject/constant/MessageType.java
+++ src/main/java/com/ggamangso/gptutorproject/constant/MessageType.java
@@ -2,16 +2,26 @@ package com.ggamangso.gptutorproject.constant;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Objects;
+
+@Getter
 public enum MessageType {
     SYSTEM("system"),
     ASSISTANT("assistant"),
     USER("user");
 
-      @Getter
-    private final String value;
+      private final String value;
 
     MessageType( String value) {
         this.value = value;
+    }
+
+    public static MessageType of(String value){
+        return Arrays.stream(values())
+                .filter(val -> Objects.equals(value, val.value))
+                .findFirst()
+                .orElse(null);
     }
 
 
