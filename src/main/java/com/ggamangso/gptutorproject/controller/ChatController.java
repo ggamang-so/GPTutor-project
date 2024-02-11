57b76d7618f1aa67ed9941b6bec589f9ccff84ea
ggamang-so <ggamangso@gmail.com>
2024-02-11 21:07:23 +0900

    채팅창에서 text나 recording 파일 입력시 message 처리관련 로직 추가
    채팅창에서 Text로 보내거나 녹음을 해서 파일로 보내면
    controller에서 받아서 처리하는 로직 추가
    - 실질적인 로직은 service 단에서 처리되어 데이터를 받도록 조정함
    
    messageService
    * 입력된 text나 STT를 통해서 변환된 text를 받아서
    OpenAI API로 보낼 때 원하는 형식으로 받을 수 있도록 프롬프트를 조정하여 request를 만드는 로직 구현

--- src/main/java/com/ggamangso/gptutorproject/controller/ChatController.java
+++ src/main/java/com/ggamangso/gptutorproject/controller/ChatController.java
@@ -1,30 +1,32 @@
 package com.ggamangso.gptutorproject.controller;
 
-import com.ggamangso.gptutorproject.config.OpenAIConfig;
 import com.ggamangso.gptutorproject.constant.MessageType;
 import com.ggamangso.gptutorproject.domain.dto.ChatDto;
 import com.ggamangso.gptutorproject.domain.dto.MessageDto;
 import com.ggamangso.gptutorproject.domain.dto.request.ChatRequest;
-import com.ggamangso.gptutorproject.domain.dto.request.MessageRequest;
 import com.ggamangso.gptutorproject.domain.dto.security.TutorPrincipal;
+import com.ggamangso.gptutorproject.repository.ChatRepository;
+import com.ggamangso.gptutorproject.repository.MessageRepository;
 import com.ggamangso.gptutorproject.service.ChatService;
 import com.ggamangso.gptutorproject.service.GoogleSTTService;
 import com.ggamangso.gptutorproject.service.MessageService;
 import com.ggamangso.gptutorproject.service.OpenAIService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
+    private final MessageRepository messageRepository;
+    private final ChatRepository chatRepository;
     private final ChatService chatService;
     private final GoogleSTTService googleSTTService;
     private final OpenAIService openAIService;
@@ -34,7 +36,7 @@ public class ChatController {
     @GetMapping("/chats")
     public String chats(ModelMap map, @AuthenticationPrincipal TutorPrincipal tutorPrincipal) {
         String userId = tutorPrincipal.getUsername();
-        System.out.println(userId);
+        log.info(userId);
         List<ChatDto> chats = chatService.searchChats(userId);
         map.addAttribute("chats", chats);
         return "chats/new";
@@ -44,55 +46,51 @@ public class ChatController {
     public String chats(@PathVariable Long chatId, ModelMap map,
                         @AuthenticationPrincipal TutorPrincipal tutorPrincipal) throws Exception {
         String userId = tutorPrincipal.getUsername();
-        if(!chatService.searchChat(chatId).userAccountDto().userId().equals(userId)){
-           return "redirect:/chats";
+        if (!chatService.searchChat(chatId).userAccountDto().userId().equals(userId)) {
+            return "redirect:/chats";
         }
         List<ChatDto> chats = chatService.searchChats(userId);
         map.addAttribute("chats", chats);
         List<MessageDto> messages = messageService.searchMessages(chatId);
         map.addAttribute("messages", messages);
+        map.addAttribute("chatId", chatId);
         return "chats/new";
     }
 
-    @PostMapping("/speaking_record_ok")
-    @ResponseBody
-    public String recodingToText(@RequestParam("file") MultipartFile file) throws Exception {
-        return openAIService.correctingContent(ChatRequest.of(
-                OpenAIConfig.MODEL,
-                List.of(MessageRequest.of(MessageType.SYSTEM.getValue(), "You are a helpful assistant."),
-                        MessageRequest.of(MessageType.USER.getValue(), googleSTTService.STTConvert(file))),
-                OpenAIConfig.MAX_TOKEN,
-                OpenAIConfig.TEMPERATURE,
-                OpenAIConfig.TOP_P
-        )).choices().get(0).message().content();
+    @GetMapping("/chats/new")
+    public String newChat(@AuthenticationPrincipal TutorPrincipal tutorPrincipal){
+        long chatId = chatService.createChat(tutorPrincipal.getUsername());
+        log.info("newChat 생성하기: " + chatId);
+        return "redirect:/chats/"+chatId;
+
     }
 
-    @GetMapping("/chats/test")
-    public String test(@RequestParam(required = false) String response, ModelMap map) {
-        if (response != null) map.addAttribute("response", response);
-        return "/chats/test";
+    @PostMapping("/api/speaking_record_ok")
+    @ResponseBody
+    public List<MessageDto> recodingToText(@RequestParam("file") MultipartFile file,
+                                           @RequestParam long chatId) throws Exception {
+        System.out.println(chatId);
+        String quest = googleSTTService.STTConvert(file); // 음성녹음을 Text로 전환
+        ChatRequest request = messageService.SetChatRequest(quest, chatId);
+        List<MessageDto> messageDtos = openAIService.correctingContent(request, quest, chatId);
+        System.out.println(MessageType.of("assistance"));
+        return messageDtos.stream()
+                .map(messageService::saveMessage)
+                .toList();
+
     }
 
     @PostMapping("/api/chats/openAI_ok")
     @ResponseBody
-    public String ChatGPT(@RequestParam String quest, ModelAndView mav) {
-        String response = openAIService.correctingContent(ChatRequest.of(
-                OpenAIConfig.MODEL,
-                List.of(MessageRequest.of(MessageType.SYSTEM.getValue(), "You are a helpful assistant."),
-                        MessageRequest.of(MessageType.USER.getValue(), quest)),
-                OpenAIConfig.MAX_TOKEN,
-                OpenAIConfig.TEMPERATURE,
-                OpenAIConfig.TOP_P
-        )).choices().get(0).message().content();
-        System.out.println(response);
-        return response;
+    public List<MessageDto> ChatGPT(@RequestParam String quest,
+                                    @RequestParam long chatId) {
+        ChatRequest request = messageService.SetChatRequest(quest, chatId);
+        List<MessageDto> messageDtos = openAIService.correctingContent(request, quest, chatId);
+        return messageDtos.stream()
+                .map(messageService::saveMessage)
+                .toList();
+
     }
 
-    @PostMapping("/chats/post_test")
-    public String post_test(@RequestHeader HttpHeaders headers, @RequestBody ChatRequest request){
-        System.out.println(request.toString());
-        System.out.println(headers.toString());
 
-        return "redirect:/chats/test";
-    }
 }
--- src/main/java/com/ggamangso/gptutorproject/service/MessageService.java
+++ src/main/java/com/ggamangso/gptutorproject/service/MessageService.java
@@ -1,8 +1,12 @@
 package com.ggamangso.gptutorproject.service;
 
+import com.ggamangso.gptutorproject.config.OpenAIConfig;
+import com.ggamangso.gptutorproject.constant.MessageType;
 import com.ggamangso.gptutorproject.domain.Chat;
 import com.ggamangso.gptutorproject.domain.Message;
 import com.ggamangso.gptutorproject.domain.dto.MessageDto;
+import com.ggamangso.gptutorproject.domain.dto.request.ChatRequest;
+import com.ggamangso.gptutorproject.domain.dto.request.MessageRequest;
 import com.ggamangso.gptutorproject.repository.ChatRepository;
 import com.ggamangso.gptutorproject.repository.MessageRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -30,15 +34,60 @@ public class MessageService {
 
 
     @Transactional
-    public void saveMessages(MessageDto messageDto) {
-        try{
+    public MessageDto saveMessage(MessageDto messageDto) {
+        Message message = null;
+        try {
             Chat chat = chatRepository.getReferenceById(messageDto.chatDto().chatId());
-            Message message = messageDto.toEntity(chat);
-            messageRepository.save(message);
+            message = messageDto.toEntity(chat);
 
-        }catch(EntityNotFoundException e){
+
+        } catch (EntityNotFoundException e) {
             log.warn("메시지 저장 실패, 메시지 작성에 필요한 정보를 찾을 수 없습니다  -{}", e.getLocalizedMessage());
         }
+        assert message != null;
+        return MessageDto.from(messageRepository.save(message));
+    }
+
+    public ChatRequest SetChatRequest(String quest, long chatId) {
+        String modifiedQuest = "";
+        if (isFirstMessage(chatId)) {
+            modifiedQuest = firstPrompt(quest);
+        }
+        if (!isFirstMessage(chatId)) {
+            modifiedQuest = promptModifying(quest);
+        }
+        List<MessageRequest> messages = new java.util.ArrayList<>(messageRepository.findByChat_ChatId(chatId)
+                .stream().map(MessageRequest::from)
+                .toList());
+        messages.add(MessageRequest.of(MessageType.USER.getValue(), modifiedQuest));
+        return ChatRequest.of(
+                OpenAIConfig.MODEL,
+                messages,
+                OpenAIConfig.MAX_TOKEN,
+                OpenAIConfig.TEMPERATURE,
+                OpenAIConfig.TOP_P
+        );
+    }
+
+    public boolean isFirstMessage(long chatId) {
+        return !(messageRepository.findByChat_ChatId(chatId).size() > 1);
+    }
+
+    public String promptModifying(String quest) {
+
+        return "Target sentence : " + quest ;
+    }
+
+    public String firstPrompt(String quest) {
+        return "Please follow these rules in future conversations:"+
+        "1. The target sentence is the sentence after 'Target Sentence :'"+
+        "2. Look at the target sentence and modify it to be more conversational expressions used in English-speaking countries."+
+        "3. If the target sentence contains profanity or inappropriate language, remove or edit it."+
+        "4. Write the corrected sentence after 'Correcting: '"+
+        "5. Write your response to the modified sentence after 'Response: '"+
+        "6. Please give your answer in conversational style, no more than 2 sentences."+
+                "Target sentence : "+quest;
+
     }
 
 }
