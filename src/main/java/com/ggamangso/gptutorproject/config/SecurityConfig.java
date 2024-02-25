fec79f5ceb81fcb35962d5a891a2a5eb5a524c21
ggamangso1 <ggamangso@gmail.com>
2024-02-26 00:22:45 +0900

    #23 - kakao 로그인을 위한 설정 및 구현
    * OAuth2 에 대한 의존성 주입
    * Security filter chain 에 oauth2login 설정
      -  카카오 로그인으로 인증 시 해당 유저에 대한 정보를 DB에 저장하는 로직 구현  및 bean 등록
    * 카카오 서버로 부터 받아 올 user에 대한 데이터를 담을 수 있는 DTO 를 record로 구현
    * 카카오 유저정보를 처리 할 수 있도록 principal에 관련 내용 추가
    * application.yaml 에 oauth2 client 관련 설정 값들을 넣어준다.
       - method에 POST라고 해서 오류가 났었는데 명칭이 변경되었으니 유의하자.
    * 카카오 로그인에 사용할 이미지 추가

--- src/main/java/com/ggamangso/gptutorproject/config/SecurityConfig.java
+++ src/main/java/com/ggamangso/gptutorproject/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ggamangso.gptutorproject.config;
 
+import com.ggamangso.gptutorproject.domain.dto.security.KakaoOAuth2Response;
 import com.ggamangso.gptutorproject.domain.dto.security.TutorPrincipal;
 import com.ggamangso.gptutorproject.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
@@ -12,26 +13,40 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+    ) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/signup/**", "/","/api/**").permitAll()
+                        .requestMatchers("/signup/**", "/", "/api/**").permitAll()
+                        .requestMatchers("/auth/find_password").permitAll()
                         .requestMatchers("/my-page/**").permitAll()
                         .requestMatchers("/favicon.ico", "/error").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(form -> form.loginPage("/loginForm").permitAll()
                         .loginProcessingUrl("/login")
                         .defaultSuccessUrl("/chats"))
-
                 .logout(logout -> logout.logoutSuccessUrl("/loginForm"))
+                .oauth2Login(oAuth -> oAuth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)
+                        )
+                )
                 .build();
 
     }
@@ -43,8 +58,42 @@ public class SecurityConfig {
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
     }
 
-        @Bean
-        public PasswordEncoder passwordEncoder() {
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            UserAccountService userAccountService,
+            PasswordEncoder passwordEncoder
+    ) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            KakaoOAuth2Response kakaoResponse = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId();
+            String providerId = String.valueOf(kakaoResponse.id());
+            String username = registrationId + "_" + providerId;
+            String dummyPassword = passwordEncoder.encode("{bcrypt}" + UUID.randomUUID());
+
+            return userAccountService.searchUser(username)
+                    .map(TutorPrincipal::from)
+                    .orElseGet(() ->
+                            TutorPrincipal.from(
+                                    userAccountService.saveUser(
+                                            username,
+                                            dummyPassword,
+                                            "ROLE_USER",
+                                            kakaoResponse.email(),
+                                            kakaoResponse.nickname(),
+                                            null
+                                    )
+                            )
+                    );
+        };
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
             return PasswordEncoderFactories.createDelegatingPasswordEncoder();
         }
 }
